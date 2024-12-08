b8d3bc618153ccc9219880ea0be8dfec0652eb1c
Talothyn <acer.tempest@gmail.com>
2024-12-07 22:58:31 -0600

    added circleshape.py

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
     x = 0
     while x == 0:
         for event in pygame.event.get():
@@ -14,5 +16,7 @@ def main():
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        game_clock.tick(60)
+        dt = game_clock.tick(60)/1000
 if __name__ == "__main__":
     main()
