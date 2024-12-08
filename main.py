984b497c49753e376a7c384c3a00cb5abb60310f
Talothyn <acer.tempest@gmail.com>
2024-12-07 23:14:08 -0600

    Drawing the player

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #Importing pygame to allow for use of the open-source pygame library
 from constants import *
 import pygame
+from player import *
 def main():
     pygame.init()
     print("Starting asteroids!")
@@ -10,11 +11,13 @@ def main():
     game_clock = pygame.time.Clock()
     dt = 0
     x = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS)
     while x == 0:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
         game_clock.tick(60)
         dt = game_clock.tick(60)/1000
