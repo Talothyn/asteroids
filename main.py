059b33db129ad224106df564fd509d7b748b90f3
Talothyn <acer.tempest@gmail.com>
2024-12-07 22:46:11 -0600

    Starting the game loop

---
+++ main.py
@@ -0,0 +1,18 @@
+#Importing pygame to allow for use of the open-source pygame library
+from constants import *
+import pygame
+def main():
+    pygame.init()
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    x = 0
+    while x == 0:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+if __name__ == "__main__":
+    main()
